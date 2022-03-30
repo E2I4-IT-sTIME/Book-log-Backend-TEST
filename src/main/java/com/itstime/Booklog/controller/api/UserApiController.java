eb72f5490e02389678b9016233983dc577e30dff
kyj <khkkgon0118@gmail.com>
2022-03-30 23:43:31 +0900

    회원가입 기능

--- src/main/java/com/itstime/Booklog/controller/api/UserApiController.java
+++ src/main/java/com/itstime/Booklog/controller/api/UserApiController.java
@@ -1,4 +1,23 @@
 package com.itstime.Booklog.controller.api;
 
+import com.itstime.Booklog.dto.JoinDto;
+import com.itstime.Booklog.model.User;
+import com.itstime.Booklog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RequiredArgsConstructor
+@Controller
 public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/auth/joinProc")
+    public String join(JoinDto joinDto) {
+        User user = joinDto.toEntity();
+        User userEntity = userService.join(user);
+        System.out.println(userEntity);
+        return "loginForm";
+    }
 }
---
+++ src/main/java/com/itstime/Booklog/dto/JoinDto.java
@@ -0,0 +1,36 @@
+package com.itstime.Booklog.dto;
+
+import com.itstime.Booklog.model.RoleType;
+import com.itstime.Booklog.model.User;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class JoinDto {
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    private String birthday;
+    private String job;
+    private String area;
+    private boolean active;
+    private RoleType role;
+
+    public User toEntity() {
+        return User.builder()
+                .username(username)
+                .password(password)
+                .username(nickname)
+                .email(email)
+                .birthday(birthday)
+                .job(job)
+                .area(area)
+                .active(true)
+                .role(RoleType.USER)
+                .build();
+    }
+}
